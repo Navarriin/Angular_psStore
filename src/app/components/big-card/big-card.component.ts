1ed0adf1c432d71484c636d4e70ba1fd119cc7c5
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-12 19:27:22 -0300

    deixando cards dinamicos cm bd fake

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
+  @Input() id: string | null = '0';
+  protected photoCover: string = '';
+  protected title: string = '';
+  protected content: string = '';
+  protected value: string = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => this.id === value.get('id'));
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null): void {
+    const result = dataFake.filter((value) => value.id === this.id)[0];
+    this.photoCover = result.photo;
+    this.title = result.title;
+    this.content = result.content;
+    this.value = result.value;
+  }
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: '1',
+    photo:
+      'https://cdn.awsli.com.br/2500x2500/1610/1610163/produto/195554057/poster-super-mario-bros-o-filme-b-f684e900.jpg',
+    title: 'THE SUPER MARIO BROSS',
+    content: 'Full Game | PS4',
+    value: '$59.90',
+  },
+  {
+    id: '2',
+    photo:
+      'https://image.api.playstation.com/vulcan/img/cfn/11307mWaEjTEJ0S9Mvb64cBPBluyR6_ZLrGrDyjnj8U50MVUD4vvRZ7vT915N8GwntDoRJosrxIcYOjlPIkLtqqH_IQI4MlI.png',
+    title: 'CALL OF DUTY BLACK OPS 3',
+    content: 'Full Game | PS4 | PS5',
+    value: '$99.90',
+  },
+  {
+    id: '3',
+    photo:
+      'https://assets-prd.ignimgs.com/2023/09/21/destiny2witch-1695322942740.jpg',
+    title: 'DESTINY 2 SEASON OF THE WITCH',
+    content: 'Full Game | PS4',
+    value: '$49.90',
+  },
+];
